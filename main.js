efa542f649fc7873b932057b729d44570d5fa408
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-03-02 14:58:51 +0200

    added main.js and made btn init

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// btn init
+const inputDigital = document.querySelector(".input-digital");
+const btnAC = document.querySelector(".btn-AC");
+const btnPlusMinus = document.querySelector(".btn-plus-minus");
+const btnPercent = document.querySelector(".btn-percent");
+const btnDevide = document.querySelector(".btn-devide");
+const btn7 = document.querySelector(".btn-7");
+const btn8 = document.querySelector(".btn-8");
+const btn9 = document.querySelector(".btn-9");
+const btnMult = document.querySelector(".btn-mult");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+const btn6 = document.querySelector(".btn-6");
+const btnMinus = document.querySelector(".btn-minus");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btnPlus = document.querySelector(".btn-plus");
+const btn0 = document.querySelector(".btn-0");
+const btnComa = document.querySelector(".btn-coma");
+const btnEquel = document.querySelector(".btn-equel");
+
+let inputTable = inputDigital.value;
+
+btnAC.addEventListener("click", () => console.log(inputDigital.value));
